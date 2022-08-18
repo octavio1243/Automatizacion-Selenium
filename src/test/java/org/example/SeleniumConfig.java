0eb2ad507140e1c71b863ad468517ba572ff258d
Tomas Espinosa <tomasespin12@gmail.com>
2022-08-17 23:04:35 -0300

    Eleccion de Browser y Sistema Operativo

--- src/test/java/org/example/SeleniumConfig.java
+++ src/test/java/org/example/SeleniumConfig.java
@@ -5,39 +5,41 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Objects;
 
 public class SeleniumConfig {
+    //"Windows" o "Linux"
+    private final String OS = "Linux";
     private WebDriver driver;
 
-    public SeleniumConfig() {
-    }
+    public SeleniumConfig(String browser) {
+        if (!(Objects.equals(browser, "chrome") || Objects.equals(browser, "firefox"))) {
+            throw new RuntimeException("Navegador inválido. No se puede crear driver");
+        }
 
-    public SeleniumConfig(String tipo){
-        String rutaProyecto=System.getProperty("user.dir"); // Obtener ruta del proyecto para localizar el driver
+        String rutaProyecto = System.getProperty("user.dir"); // Obtener ruta del proyecto para localizar el driver
+        System.out.println("Creando driver para " + browser);
 
-        if (tipo=="chrome"){
+        if (Objects.equals(browser, "chrome")) {
             // Crear Driver para Chrome
-            System.out.println("Creando driver para chrome");
-            System.setProperty("webdriver.chrome.driver",rutaProyecto+"\\selenium\\chromedriver.exe");
-            this.driver=new ChromeDriver();
-        }
-        else if (tipo=="firefox"){
+            String rutaDriver = (OS == "Windows" ? "\\selenium\\chromedriver.exe" : "/selenium/chromedriver");
+            System.setProperty("webdriver.chrome.driver", rutaProyecto + rutaDriver);
+            this.driver = new ChromeDriver();
+        } else if (Objects.equals(browser, "firefox")) {
             // Crear Driver para firefox
-            System.out.println("Creando driver para firefox");
-            System.setProperty("webdriver.gecko.driver",rutaProyecto+"\\selenium\\geckodriver.exe");
-            this.driver=new FirefoxDriver();
+            String rutaDriver = (OS == "Windows" ? "\\selenium\\geckodriver.exe" : "/selenium/geckodriver");
+            System.setProperty("webdriver.gecko.driver", rutaProyecto + rutaDriver);
+            this.driver = new FirefoxDriver();
         }
-        else{
-            System.out.println("Navegador inválido. No se puede crear driver");
-        }
+
         this.configBasic();
     }
 
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return driver;
     }
 
-    public void configBasic(){
+    public void configBasic() {
         this.driver.manage().window().maximize();
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Espera de 10 segundos para que aparezca un elemento
     }
